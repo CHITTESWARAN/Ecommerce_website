52caaa1fc7b404f5bcb3c089328d4dfc0ccb46fa
CHITTESWARAN <chitteswarancj06@gmail.com>
2024-09-28 14:52:45 +0530

    thefirstcommit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from './Banner';
 import Category from './Category';
 import Products from './Products';
@@ -11,6 +11,15 @@ import Footer from './Footer';
 
 
 const Home = () => {
+   
+    const [count,setCount]=(0);
+  useEffect(() => {
+   
+    setCount((prev) => prev + 1);
+    localStorage.setItem('user', JSON.stringify(count));
+    let val = JSON.parse(localStorage.getItem('user'));
+    console.log('Stored value:', val);
+  }, [ ]);
   return (
     <>
     <Banner/>
